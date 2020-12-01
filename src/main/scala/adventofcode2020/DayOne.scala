29f9bf49d6e9e6a5dd8c65910c1478112fe481bf
Phillip Perks <p.perks@elsevier.com>
2020-12-01 11:20:49 +0000

    day one recursive solution

--- src/main/scala/adventofcode2020/DayOne.scala
+++ src/main/scala/adventofcode2020/DayOne.scala
@@ -1,5 +1,6 @@
 package adventofcode2020
 
+import scala.annotation.tailrec
 import scala.io.Source
 
 object DayOne {
@@ -7,8 +8,8 @@ object DayOne {
   def main(args: Array[String]): Unit = {
     val input = Source.fromResource("day-1-part-1.txt").getLines().toSeq.map(_.toInt)
 
-    val partOneAnswer = productOfNumbersThatAddTo2020(input, 2)
-    val partTwoAnswer = productOfNumbersThatAddTo2020(input, 3)
+    val partOneAnswer = recursiveProductOfNumbersThatAddTo2020(input, 2)
+    val partTwoAnswer = recursiveProductOfNumbersThatAddTo2020(input, 3)
 
     println(s"Part One: $partOneAnswer")
     println(s"Part Two: $partTwoAnswer")
@@ -16,6 +17,24 @@ object DayOne {
 
   def productOfNumbersThatAddTo2020(input: Seq[Int], n: Int): Int = {
     input.combinations(n).filter(l => l.sum == 2020).toSeq.head.product
+  }
+
+  def recursiveProductOfNumbersThatAddTo2020(input: Seq[Int], n: Int): Int = {
+    val combinations = input.combinations(n).toSeq
+
+    @tailrec
+    def productOfNumbersThatAddTo2020(combinations: Seq[Seq[Int]]): Int = {
+      val head = combinations.head
+      if(head.sum == 2020){
+        head.product
+      }
+      else{
+        productOfNumbersThatAddTo2020(combinations.tail)
+      }
+    }
+
+    productOfNumbersThatAddTo2020(combinations)
+
   }
 
 }
